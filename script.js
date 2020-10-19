a17c243fbfb74ff5240c5fe53b0cff2228d4f03d
Shanmuk Mayur A <shanmuk.crisil@gmail.com>
2020-10-18 23:22:11 -0400

    Countdown-timer-app

---
+++ script.js
@@ -0,0 +1,28 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
+
+const newYear = "4 August 2021";
+
+function countdown() {
+  const newYearDate = new Date(newYear);
+  const currentDate = new Date();
+
+  const totalSeconds = (newYearDate - currentDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minutesEl.innerHTML = minutes;
+  secondsEl.innerHTML = seconds;
+}
+
+function formatTime(time) {
+  return time < 10 ? `10${time}` : time;
+}
+countdown();
+setInterval(countdown, 1000);
