06564a7a09faf671f1ddbc49248af7109a7ecae0
Shanmuk Mayur A <shanmuk.crisil@gmail.com>
2020-10-19 00:00:20 -0400

    Update the date

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
 
-const newYear = "4 August 2021";
+const newYear = "26 January 2021";
 
 function countdown() {
   const newYearDate = new Date(newYear);
